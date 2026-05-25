842d861c49332924385aff67f3bf86580fb39916
TM-Tareq <tmtareq20@gmail.com>
2026-05-25 09:46:09 +0600

    First commit of this project with dependency setup of backend

---
+++ src/test/java/com/tm/pos_system/PosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tm.pos_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
