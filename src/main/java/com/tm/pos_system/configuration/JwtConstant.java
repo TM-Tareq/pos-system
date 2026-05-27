10e38a5b6c13a63b0fad5d5f6da36e3189bb1b86
TM-Tareq <tmtareq20@gmail.com>
2026-05-27 14:13:09 +0600

    JWT secured

---
+++ src/main/java/com/tm/pos_system/configuration/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.tm.pos_system.configuration;
+
+public class JwtConstant {
+    public static final String JWT_SECRET = "alfjlsjfojsfsjlfslfsifsdfsflalksjfiosijffisdfkjfdifdidk";
+    public static final String JWT_HEADER = "Authorization";
+//    public static final String JWT_TOKEN_PREFIX = "Bearer ";
+}
